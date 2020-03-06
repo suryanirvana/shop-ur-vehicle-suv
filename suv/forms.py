043b7e714cd65ecf030996a76317f4a2b1d90c79
Surya Nirvana <suryanirvanaprima@gmail.com>
2020-03-06 21:22:39 +0700

    Connect models and views for Review

--- suv/forms.py
+++ suv/forms.py
@@ -7,6 +7,49 @@ CAR_TYPE = [
     ('SEDAN','SEDAN'),
 ]
 
+class ReviewForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'maxlength' : '1000',
+    }))
+    car = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'maxlength' : '1000',
+    }))
+    message = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'maxlength' : '1000',
+    }))
+    created_date = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'maxlength' : '1000',
+    }))
+    rating = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'maxlength' : '100',
+    }))
+
+# class TransactionForm(forms.Form):
+#     username = forms.CharField(widget=forms.TextInput(attrs=
+#         'class' : 'form-control',
+#         'maxlength' : '1000',
+#     ))
+#     car = forms.CharField(widget=forms.TextInput(attrs=
+#         'class' : 'form-control',
+#         'maxlength' : '1000',
+#     ))
+#     message = forms.CharField(widget=forms.TextInput(attrs=
+#         'class' : 'form-control',
+#         'maxlength' : '1000',
+#     ))
+#     created_date = forms.CharField(widget=forms.TextInput(attrs=
+#         'class' : 'form-control',
+#         'maxlength' : '1000',
+#     ))
+#     rating = forms.CharField(widget=forms.TextInput(attrs=
+#         'class' : 'form-control',
+#         'maxlength' : '100',
+#     ))
 class CarForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={
         'class' : 'form-control',
--- suv/views.py
+++ suv/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render,redirect
 from .models import *
 from .forms import *
+import datetime
 
 # Create your views here.
 def index(request):
     response = {}
     return render(request,'index.html',response)
+
+def review(request):
+    if request.method == "POST":
+        req = request.POST
+        form = ReviewForm(request.POST)
+        print("\nIni Request :",req)
+
+        new_car = Car.objects.get(name=req['year'])
+        new_car.save()
+
+        new_review = Review.objects.create(username=req['name'],car=new_car,message = req['location'],created_date = datetime.datetime.today(),rating = req['owner'])
+        new_review.save()
+        return redirect('/review.html')
+    else:
+        form = ReviewForm()
+    reviews = Review.objects.all()
+    car = Car.objects.all()
+    response = {'reviews' : reviews, 'car' : car}
+    return render(request, 'review.html', response)
 
 def rent(request):
     if request.method == "POST":
@@ -23,25 +43,7 @@ def rent(request):
     cars = Car.objects.all()
     car_type = Category.objects.all()
     response = {'cars' : cars, 'car_type' : car_type}
-    return render(request, 'rent.html', response)
-
-def review(request):
-    if request.method == "POST":
-        req = request.POST
-        form = CarForm(request.POST, request.FILES)
-
-        type_car = Category.objects.get_or_create(car_type=req['car_type'])[0]
-        type_car.save()
-
-        new_car = Car.objects.create(name=req['name'],category=type_car,year = req['year'],city = req['location'],username = req['owner'],car_image = req['car_image'])
-        new_car.save()
-        return redirect('/rent.html')
-    else:
-        form = CarForm()
-    cars = Car.objects.all()
-    car_type = Category.objects.all()
-    response = {'cars' : cars, 'car_type' : car_type}
-    return render(request,'review.html',response)
+    return render(request,'rent.html',response)
 
 def article(request):
     response = {'article_list':Article.objects.all().values()}
